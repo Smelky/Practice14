fd25cd76329a7ce0b942d61de98d912c7ae59906
Smelky <46086711+Smelky@users.noreply.github.com>
2019-04-13 09:52:04 +0300

    Delete Main class

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import abstractfactory.AbstractFactory;
-
-public class Main {
-    public static void main(String[] args) {
-        AbstractFactory abstractFactory = new AbstractFactory();
-        abstractFactory.createFactory(null);
-    }
-}
