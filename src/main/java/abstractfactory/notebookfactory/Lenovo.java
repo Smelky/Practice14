96d5bb91bb373e9f350b5b2f96242beae6fc1c8a
Smelky <46086711+Smelky@users.noreply.github.com>
2019-04-13 11:52:49 +0300

    Added one blank line

--- src/main/java/abstractfactory/notebookfactory/Lenovo.java
+++ src/main/java/abstractfactory/notebookfactory/Lenovo.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger;
 public class Lenovo implements Notebook {
 
     private static final Logger LOGGER = Logger.getLogger(Lenovo.class);
-    
+
     @Override
     public void work() {
         LOGGER.info("Samsung is working");
